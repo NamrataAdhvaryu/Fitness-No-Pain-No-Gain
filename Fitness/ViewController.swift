60ddc626acbae48b524cb74908ea0a3721e992e1
Namrata <namrata.adhvaryu05@gmail.com>
2020-08-26 22:10:51 -0700

    Initial Commit

---
+++ Fitness/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fitness
+//
+//  Created by Namrata Akash on 26/08/20.
+//  Copyright © 2020 namrata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
